13e6893174a20c0a78d63c4b17c41bea5e8a932b
Alejo <alejovillores@gmail.com>
2020-12-24 10:33:11 -0300

    fix: saque el // TODO:

--- src/main/java/fiuba/algo3/modelo/Movimiento.java
+++ src/main/java/fiuba/algo3/modelo/Movimiento.java
@@ -2,7 +2,6 @@ package fiuba.algo3.modelo;
 
 public abstract class Movimiento {
     protected Estado estado;
-    // TODO: VER COMO GUARDAMOS LOS VALORES DE LOS MOVIMIENTOS
 
     public boolean estaDibujado(){
         return estado.estaDibujado();
