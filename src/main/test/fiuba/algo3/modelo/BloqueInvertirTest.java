2b864338b8bf78fb5e23ccebe327da2bc6bf0594
root-MKevin <kmeaca@fi.uba.ar>
2021-01-06 20:20:26 -0300

    .) Se crea una rama nueva para los bloques que heredan del decorator.
    .) Los bloques repetir estan creados exitosamente
    
    .) Los test de los bloques repetir pasan todos
    
    .) El bloque invertir queda pendiente a ser modificado por problemas de diseño.

---
+++ src/main/test/fiuba/algo3/modelo/BloqueInvertirTest.java
@@ -0,0 +1,65 @@
+package fiuba.algo3.modelo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import fiuba.algo3.modelo.bloques.*;
+
+public class BloqueInvertirTest {
+
+    @Test
+    public void test01ElBloqueInvertirLanzaLaExcepcionDeBloquePersonalizadoAlEjecutarseSinNingunBloqueDentro()
+    {
+        BloquePersonalizado bloquePersonalizado = new BloquePersonalizado();
+        BloqueInvertir bloqueInvertir = new BloqueInvertir(bloquePersonalizado);
+        SectorDibujo sectorDibujo = new SectorDibujo();
+
+        assertThrows(BloquePersonalizadoNoPuedeEjecutarseSinBloquesError.class,
+                () -> bloqueInvertir.ejecutar(sectorDibujo));
+    }
+
+    @Test
+    public void test02AgregarUnBloqueAlBloqueInvertirYEjecutarloRealizaLaOperaciónInversaDelBloqueInterior()
+    {
+        BloquePersonalizado bloquePersonalizado = new BloquePersonalizado();
+        SectorDibujo sectorDibujoMock = mock(SectorDibujo.class);
+        BloqueMovimiento bloqueMovimientoMock = mock(BloqueMovimiento.class);
+
+        bloquePersonalizado.agregar(bloqueMovimientoMock);
+
+        BloqueInvertir bloqueInvertir = new BloqueInvertir(bloquePersonalizado);
+
+        bloqueInvertir.ejecutar(sectorDibujoMock);
+
+        verify(bloqueMovimientoMock,times(1)).invertirMovimiento();
+        verify(bloqueMovimientoMock,times(1)).ejecutar(sectorDibujoMock);
+    }
+
+
+    @Test
+    public void test03EjecutarElBloqueInvertirConDosBloquesInterioresRealizaLaOperaciónInversaEnCadaUnoDeEllos()
+    {
+        BloquePersonalizado bloquePersonalizado = new BloquePersonalizado();
+        SectorDibujo sectorDibujoMock = mock(SectorDibujo.class);
+        BloqueMovimiento bloqueMovimientoMock = mock(BloqueMovimiento.class);
+        BloqueMovimiento otroMovimientoMock = mock(BloqueMovimiento.class);
+
+        bloquePersonalizado.agregar(bloqueMovimientoMock);
+        bloquePersonalizado.agregar(otroMovimientoMock);
+
+        BloqueInvertir bloqueInvertir = new BloqueInvertir(bloquePersonalizado);
+
+        bloqueInvertir.ejecutar(sectorDibujoMock);
+
+        verify(bloqueMovimientoMock,times(1)).invertirMovimiento();
+        verify(bloqueMovimientoMock,times(1)).ejecutar(sectorDibujoMock);
+
+        verify(otroMovimientoMock,times(1)).invertirMovimiento();
+        verify(otroMovimientoMock,times(1)).ejecutar(sectorDibujoMock);
+
+    }
+}
---
+++ src/main/test/fiuba/algo3/modelo/BloqueRepetirDosVecesTest.java
@@ -0,0 +1,132 @@
+package fiuba.algo3.modelo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import fiuba.algo3.modelo.bloques.*;
+
+public class BloqueRepetirDosVecesTest {
+
+    @Test
+    public void test01ElBloqueRepetirDosVecesLanzaLaExcepcionDeBloquePersonalizadoAlEjecutarseSinNingunBloqueDentro()
+    {
+        BloquePersonalizado bloque = new BloquePersonalizado();
+        BloqueRepetirDosVeces bloqueRepetir = new BloqueRepetirDosVeces(bloque);
+        SectorDibujo sectorDibujo = new SectorDibujo();
+
+        assertThrows(BloquePersonalizadoNoPuedeEjecutarseSinBloquesError.class,
+                () -> bloqueRepetir.ejecutar(sectorDibujo));
+    }
+
+    @Test
+    public void test02AlEjecutarElBloqueRepetirDosVecesConUnBloqueSoloEsteUltimoSeEjecutaDosVeces()
+    {
+        BloquePersonalizado bloquePersonalizado = new BloquePersonalizado();
+
+        BloqueMovimiento bloqueMovimientoMock = mock(BloqueMovimiento.class);
+        SectorDibujo sectorDibujoMock = mock(SectorDibujo.class);
+
+        bloquePersonalizado.agregar(bloqueMovimientoMock);
+        BloqueRepetirDosVeces bloqueRepetir = new BloqueRepetirDosVeces(bloquePersonalizado);
+
+        bloqueRepetir.ejecutar(sectorDibujoMock);
+
+        verify(bloqueMovimientoMock,times(2)).ejecutar(sectorDibujoMock);
+    }
+
+
+    @Test
+    public void test03AlEjecutarDosVecesElBloqueRepetirConUnBloqueSoloEsteUltimoSeEjecutaCuatroVeces()
+    {
+        BloquePersonalizado bloquePersonalizado = new BloquePersonalizado();
+
+        BloqueMovimiento bloqueMovimientoMock = mock(BloqueMovimiento.class);
+        SectorDibujo sectorDibujoMock = mock(SectorDibujo.class);
+
+        bloquePersonalizado.agregar(bloqueMovimientoMock);
+        BloqueRepetirDosVeces bloqueRepetir = new BloqueRepetirDosVeces(bloquePersonalizado);
+
+        bloqueRepetir.ejecutar(sectorDibujoMock);
+        bloqueRepetir.ejecutar(sectorDibujoMock);
+
+        verify(bloqueMovimientoMock,times(4)).ejecutar(sectorDibujoMock);
+    }
+
+    @Test
+    public void test04AlEjecutarUnaVezElBloqueRepetirConDosBloquesSeEjecutaCadaUnoDosVeces()
+    {
+
+        BloquePersonalizado bloquePersonalizado = new BloquePersonalizado();
+
+        BloqueMovimiento movimientoMock = mock(BloqueMovimiento.class);
+        BloqueMovimiento otroMovimientoMock = mock(BloqueMovimiento.class);
+        SectorDibujo sectorDibujoMock = mock(SectorDibujo.class);
+
+        bloquePersonalizado.agregar(movimientoMock);
+        bloquePersonalizado.agregar(otroMovimientoMock);
+        BloqueRepetirDosVeces bloqueRepetir = new BloqueRepetirDosVeces(bloquePersonalizado);
+
+        bloqueRepetir.ejecutar(sectorDibujoMock);
+
+        verify(movimientoMock,times(2)).ejecutar(sectorDibujoMock);
+        verify(otroMovimientoMock,times(2)).ejecutar(sectorDibujoMock);
+    }
+
+    @Test
+    public void test05AlEjecutarDosVecesElBloqueRepetirConDosBloquesSeEjecutaCadaUnoCuatroVeces()
+    {
+        BloquePersonalizado bloquePersonalizado = new BloquePersonalizado();
+
+        BloqueMovimiento movimientoMock = mock(BloqueMovimiento.class);
+        BloqueMovimiento otroMovimientoMock = mock(BloqueMovimiento.class);
+        SectorDibujo sectorDibujoMock = mock(SectorDibujo.class);
+
+        bloquePersonalizado.agregar(movimientoMock);
+        bloquePersonalizado.agregar(otroMovimientoMock);
+        BloqueRepetirDosVeces bloqueRepetir = new BloqueRepetirDosVeces(bloquePersonalizado);
+
+        bloqueRepetir.ejecutar(sectorDibujoMock);
+        bloqueRepetir.ejecutar(sectorDibujoMock);
+
+        verify(movimientoMock,times(4)).ejecutar(sectorDibujoMock);
+        verify(otroMovimientoMock,times(4)).ejecutar(sectorDibujoMock);
+    }
+
+    @Test
+    public void test06TenerUnBloqueRepetirDosVecesDentroDeOtroBloqueRepetirDosVecesSeEjecutaCuatroVeces()
+    {
+        BloquePersonalizado bloquePersonalizado = new BloquePersonalizado();
+        BloqueMovimiento bloqueMovimientoMock = mock(BloqueMovimiento.class);
+        SectorDibujo sectorDibujoMock = mock(SectorDibujo.class);
+
+        bloquePersonalizado.agregar(bloqueMovimientoMock);
+
+        BloqueRepetirDosVeces bloqueRepetir = new BloqueRepetirDosVeces(bloquePersonalizado);
+        BloqueRepetirDosVeces otroBloqueRepetir = new BloqueRepetirDosVeces(bloqueRepetir);
+
+        otroBloqueRepetir.ejecutar(sectorDibujoMock);
+
+        verify(bloqueMovimientoMock,times(4)).ejecutar(sectorDibujoMock);
+    }
+
+    @Test
+    public void test07TenerUnBloqueRepetirTresVecesDentroDeUnBloqueRepetirDosVecesSeEjecutaSeisVeces()
+    {
+        BloquePersonalizado bloquePersonalizado = new BloquePersonalizado();
+        BloqueMovimiento bloqueMovimientoMock = mock(BloqueMovimiento.class);
+        SectorDibujo sectorDibujoMock = mock(SectorDibujo.class);
+
+        bloquePersonalizado.agregar(bloqueMovimientoMock);
+
+        BloqueRepetirTresVeces bloqueRepetirTresVeces = new BloqueRepetirTresVeces(bloquePersonalizado);
+        BloqueRepetirDosVeces bloqueRepetirDosVeces = new BloqueRepetirDosVeces(bloqueRepetirTresVeces);
+
+        bloqueRepetirDosVeces.ejecutar(sectorDibujoMock);
+
+        verify(bloqueMovimientoMock,times(6)).ejecutar(sectorDibujoMock);
+    }
+}
