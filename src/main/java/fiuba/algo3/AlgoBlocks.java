9a632f8a6d6557849cd7ace744d54c452f8ba46c
AlexTeper99 <67521917+AlexTeper99@users.noreply.github.com>
2021-01-17 23:53:01 -0300

    feat AlgoBlocksTest
    tests: Cree los test AgregarBloqueMovimientoXXx en AlgoBlocks test y agregarBloqueLevantar/BajarLapiz

--- src/main/java/fiuba/algo3/AlgoBlocks.java
+++ src/main/java/fiuba/algo3/AlgoBlocks.java
@@ -1,9 +1,6 @@
 package fiuba.algo3;
 
-import fiuba.algo3.modelo.Algoritmo;
-import fiuba.algo3.modelo.FabricaAbstractaDeBloques;
-import fiuba.algo3.modelo.FabricaConcretaBloqueQueNoDibuja;
-import fiuba.algo3.modelo.SectorDibujo;
+import fiuba.algo3.modelo.*;
 import fiuba.algo3.modelo.bloques.BloqueMovimiento;
 
 public class AlgoBlocks {
@@ -15,13 +12,41 @@ public class AlgoBlocks {
         fabricaDeBloques = new FabricaConcretaBloqueQueNoDibuja();
     }
 
-    public void agregarBloqueMovimientoArriba(){
 
+    public void agregarBloqueMovimientoArriba(){
         algoritmo.agregar(fabricaDeBloques.crearBloqueMovimientoArriba());
+    }
+
+    public void agregarBloqueMovimientoAbajo(){
+        algoritmo.agregar(fabricaDeBloques.crearBloqueMovimientoAbajo());
+    }
+
+    public void agregarBloqueMovimientoDerecha(){
+        algoritmo.agregar(fabricaDeBloques.crearBloqueMovimientoDerecha());
+    }
+
+    public void agregarBloqueMovimientoIzquierda(){
+        algoritmo.agregar(fabricaDeBloques.crearBloqueMovimientoIzquierda());
+    }
+
+    public void agregarBloqueLevantarLapiz(){
+        this.fabricaDeBloques = new FabricaConcretaBloqueQueNoDibuja();
+    }
+
+    public void agregarBloqueBajarLapiz(){
+        this.fabricaDeBloques = new FabricaConcretaBloqueQueDibuja();
     }
 
     public void ejecutar(SectorDibujo sectorDibujo){ //actualizar que recibe un parametro en el UML
         algoritmo.ejecutar(sectorDibujo);
+    }
+
+    public Algoritmo obtenerAlgoritmo(){
+        return this.algoritmo;
+    }
+
+    public FabricaAbstractaDeBloques obtenerFabrica(){
+        return this.fabricaDeBloques;
     }
 
 
--- src/main/test/fiuba/algo3/modelo/AlgoBlocksTest.java
+++ src/main/test/fiuba/algo3/modelo/AlgoBlocksTest.java
@@ -16,16 +16,64 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class AlgoBlocksTest {
     @Test
-    public void test01AgregarBloqueMovimientoArribaAlAlgoritmoLoAgrega(){
+    public void test01AgregarBloqueMovimientoArribaAgregaUnBloqueMovimientoArriba(){
 
         AlgoBlocks algoBlocks = new AlgoBlocks();
         algoBlocks.agregarBloqueMovimientoArriba();
+        FabricaConcretaBloqueQueNoDibuja fabricaNoDibuja = new FabricaConcretaBloqueQueNoDibuja();
 
-        /*
-        SectorDibujo sectorDibujo = new SectorDibujo();
-        algoBlocks.ejecutar(sectorDibujo);
-        */
+        assertEquals(algoBlocks.obtenerAlgoritmo().obtenerBloques().get(0).getClass(), fabricaNoDibuja.crearBloqueMovimientoArriba().getClass());
+    }
 
+    @Test
+    public void test02AgregarBloqueMovimientoAbajoAgregaUnBloqueMovimientoAbajo(){
 
+        AlgoBlocks algoBlocks = new AlgoBlocks();
+        algoBlocks.agregarBloqueMovimientoAbajo();
+        FabricaConcretaBloqueQueNoDibuja fabricaNoDibuja = new FabricaConcretaBloqueQueNoDibuja();
+
+        assertEquals(algoBlocks.obtenerAlgoritmo().obtenerBloques().get(0).getClass(), fabricaNoDibuja.crearBloqueMovimientoAbajo().getClass());
+    }
+
+    @Test
+    public void test03AgregarBloqueMovimientoDerechaAgregaUnBloqueMovimientoDerecha(){
+
+        AlgoBlocks algoBlocks = new AlgoBlocks();
+        algoBlocks.agregarBloqueMovimientoDerecha();
+        FabricaConcretaBloqueQueNoDibuja fabricaNoDibuja = new FabricaConcretaBloqueQueNoDibuja();
+
+        assertEquals(algoBlocks.obtenerAlgoritmo().obtenerBloques().get(0).getClass(), fabricaNoDibuja.crearBloqueMovimientoDerecha().getClass());
+    }
+
+    @Test
+    public void test04AgregarBloqueMovimientoIzquierdaAgregaUnBloqueMovimientoIzquierda(){
+
+        AlgoBlocks algoBlocks = new AlgoBlocks();
+        algoBlocks.agregarBloqueMovimientoIzquierda();
+        FabricaConcretaBloqueQueNoDibuja fabricaNoDibuja = new FabricaConcretaBloqueQueNoDibuja();
+
+        assertEquals(algoBlocks.obtenerAlgoritmo().obtenerBloques().get(0).getClass(), fabricaNoDibuja.crearBloqueMovimientoIzquierda().getClass());
+    }
+
+    @Test
+    public void test05AgregarBloqueLevantarLapizCambiaDeFabricaANoDibuja(){
+
+        AlgoBlocks algoBlocks = new AlgoBlocks();
+        FabricaConcretaBloqueQueNoDibuja fabricaNoDibuja = new FabricaConcretaBloqueQueNoDibuja();
+
+        algoBlocks.agregarBloqueLevantarLapiz();
+
+        assertEquals(algoBlocks.obtenerFabrica().getClass(), fabricaNoDibuja.getClass());
+    }
+
+    @Test
+    public void test06AgregarBloqueBajarLapizCambiaDeFabricaDibuja(){
+
+        AlgoBlocks algoBlocks = new AlgoBlocks();
+        FabricaConcretaBloqueQueDibuja fabricaDibuja = new FabricaConcretaBloqueQueDibuja();
+
+        algoBlocks.agregarBloqueBajarLapiz();
+
+        assertEquals(algoBlocks.obtenerFabrica().getClass(), fabricaDibuja.getClass());
     }
 }
