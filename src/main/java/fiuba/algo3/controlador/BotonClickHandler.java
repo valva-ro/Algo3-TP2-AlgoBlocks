5792881b8473935ff58fc4610c9d6625b182d358
valva-ro <vavarela.rodriguez@gmail.com>
2021-02-26 18:47:29 -0300

    Arreglar sonidos al hacer click

---
+++ src/main/java/fiuba/algo3/controlador/BotonClickHandler.java
@@ -0,0 +1,16 @@
+package fiuba.algo3.controlador;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.media.AudioClip;
+
+public class BotonClickHandler implements EventHandler {
+
+    //TODO: arreglar esto e implementar comportamiento
+    //private static final AudioClip sonidoClick = new AudioClip("click_boton.mp3");
+
+    @Override
+    public void handle(Event event) {
+        //sonidoClick.play();
+    }
+}
---
+++ src/main/java/fiuba/algo3/controlador/BotonDragHandler.java
@@ -0,0 +1,15 @@
+package fiuba.algo3.controlador;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+
+public class BotonDragHandler implements EventHandler {
+
+    //TODO: arreglar esto e implementar comportamiento
+    //private static final AudioClip sonidoClick = new AudioClip("drag_bloque.mp3");
+
+    @Override
+    public void handle(Event event) {
+        //sonidoClick.play();
+    }
+}
