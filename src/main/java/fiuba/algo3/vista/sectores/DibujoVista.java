cbf166283942f8ce02c977af5233860b644345d8
valva-ro <vavarela.rodriguez@gmail.com>
2021-03-02 19:28:34 -0300

    refactor: Agregue constante para el largo del trazo

--- src/main/java/fiuba/algo3/vista/sectores/DibujoVista.java
+++ src/main/java/fiuba/algo3/vista/sectores/DibujoVista.java
@@ -3,6 +3,7 @@ package fiuba.algo3.vista.sectores;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import fiuba.algo3.modelo.Posicion;
+
 import static javafx.scene.paint.Color.BLACK;
 import static javafx.scene.paint.Color.TRANSPARENT;
 
@@ -17,32 +18,33 @@ public class DibujoVista extends Canvas {
         this.xCentro = (int) ((this.getWidth()) / 2);
         this.yCentro = (int) ((this.getHeight()) / 2);
         this.graficador = this.getGraphicsContext2D();
+        this.graficador.setLineWidth(5);
     }
 
     private void dibujarMovimientos(Posicion posicionInicial, Posicion posicionFinal) {
-        int xI = this.xCentro + (posicionInicial.obtenerX() * 20);
-        int yI = (this.yCentro + (posicionInicial.obtenerY() * (-20)) );
-        int xF = this.xCentro + (posicionFinal.obtenerX() * 20);
-        int yF = (this.yCentro + (posicionFinal.obtenerY() * (-20)) );
+        int constanteAlargarTrazo = 40;
+        int xI = this.xCentro + (posicionInicial.obtenerX() * constanteAlargarTrazo);
+        int yI = (this.yCentro + (posicionInicial.obtenerY() * (-constanteAlargarTrazo)));
+        int xF = this.xCentro + (posicionFinal.obtenerX() * constanteAlargarTrazo);
+        int yF = (this.yCentro + (posicionFinal.obtenerY() * (-constanteAlargarTrazo)));
         this.graficador.strokeLine(xI, yI, xF, yF);
     }
 
-    public void dibujar(Posicion posicionInicial, Posicion posicionFinal){
+    public void dibujar(Posicion posicionInicial, Posicion posicionFinal) {
         this.graficador.setStroke(BLACK);
-        this.graficador.setLineWidth(5);
         this.dibujarMovimientos(posicionInicial, posicionFinal);
     }
 
-    public void noDibujar(Posicion posicionInicial, Posicion posicionFinal){
+    public void noDibujar(Posicion posicionInicial, Posicion posicionFinal) {
         this.graficador.setStroke(TRANSPARENT);
         this.dibujarMovimientos(posicionInicial, posicionFinal);
     }
 
-    public void borrar(){
+    public void borrar() {
         double ancho, alto;
         ancho = graficador.getCanvas().getWidth();
         alto = graficador.getCanvas().getHeight();
-        this.graficador.clearRect(0,0,ancho,alto);
+        this.graficador.clearRect(0, 0, ancho, alto);
         //TODO: Intentar volver a la posicion original.
     }
 }
