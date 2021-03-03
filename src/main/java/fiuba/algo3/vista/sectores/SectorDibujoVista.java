f63e61c501bb2430ead7ada2780a1890741c0e93
valva-ro <vavarela.rodriguez@gmail.com>
2021-03-03 16:25:51 -0300

    no funciona

--- src/main/java/fiuba/algo3/vista/sectores/SectorDibujoVista.java
+++ src/main/java/fiuba/algo3/vista/sectores/SectorDibujoVista.java
@@ -4,12 +4,10 @@ import fiuba.algo3.modelo.ObservadorDibujo;
 import fiuba.algo3.modelo.Posicion;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 
 public class SectorDibujoVista extends BorderPane implements ObservadorDibujo {
 
     private final DibujoVista dibujo;
-    private SectorPersonaje sectorPersonaje;
     private final int ANCHO = 480,
                       ALTO = 640;
 
@@ -22,13 +20,9 @@ public class SectorDibujoVista extends BorderPane implements ObservadorDibujo {
         etiqueta.getStyleClass().add("subtitulo");
 
         this.dibujo = new DibujoVista(ANCHO, ALTO);
-        this.sectorPersonaje = new SectorPersonaje("personaje_abajo_sin_lapiz.png", ANCHO, ALTO);
-
-        Pane panel = new Pane();
-        panel.getChildren().addAll(dibujo, sectorPersonaje);
 
         this.setTop(etiqueta);
-        this.setCenter(panel);
+        this.setCenter(this.dibujo);
     }
 
     @Override
--- src/main/java/fiuba/algo3/vista/sectores/SectorPersonaje.java
+++ src/main/java/fiuba/algo3/vista/sectores/SectorPersonaje.java
@@ -32,7 +32,7 @@ public class SectorPersonaje extends GridPane {
 
             if (getColumnIndex(node) == posicionFinal.obtenerX() && getRowIndex(node) == posicionFinal.obtenerY()) {
                 this.cambiarImagen(dibuja, posicionInicial, posicionFinal);
-                BackgroundSize tamanioImagen = new BackgroundSize(50, 50, true, true, false, true);
+                BackgroundSize tamanioImagen = new BackgroundSize(40, 40, true, true, false, true);
                 BackgroundImage imagenDeFondo = new BackgroundImage(this.imagen, null, null, null, tamanioImagen);
                 ((Pane) node).setBackground(new Background(imagenDeFondo));
             }
@@ -48,8 +48,11 @@ public class SectorPersonaje extends GridPane {
                 Pane celda = new Pane();
                 celda.setMaxSize(TAMANIO_CELDA, TAMANIO_CELDA);
                 celda.setMinSize(TAMANIO_CELDA, TAMANIO_CELDA);
+                celda.setBackground(Background.EMPTY);
                 if (i == columnas / 2 && j == filas / 2) {
-                    celda.setBackground(new Background(new BackgroundImage(this.imagen, null, null, null, null)));
+                    BackgroundSize tamanioImagen = new BackgroundSize(40, 40, true, true, false, true);
+                    BackgroundImage imagenDeFondo = new BackgroundImage(this.imagen, null, null, null, tamanioImagen);
+                    celda.setBackground(new Background(imagenDeFondo));
                 }
                 this.add(celda, i, j);
             }
